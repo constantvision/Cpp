40811915dbd5f99d77cf927c6cf97c8f44749b26
Domian <96786212+constantvision@users.noreply.github.com>
2022-04-06 15:32:47 +0900

    Add files via upload

---
+++ Day3/1_정적멤버5.cpp
@@ -0,0 +1,28 @@
+﻿// 파일 분할
+class Car
+{
+	int color;
+	static int cnt;
+
+public:
+	Car();
+	~Car(); 
+
+	static int get_count();
+
+};
+
+// Car.cpp
+#include "Car.h"
+int Car::cnt = 0;
+
+Car::Car() { ++cnt; }
+Car::~Car() { --cnt; }
+
+// Static 멤버 함수를 외부에 구현시 "static"은 표기 안함
+int Car::get_count()
+{
+	return cnt;
+}
+
+
---
+++ Day3/2_this2-2.cpp
@@ -0,0 +1,32 @@
+// 2_this2-2
+#include <iostream>
+
+struct Point 
+{
+	int x;
+	int y;
+	~Point() { std::cout << "~Point()" << std::endl; }
+};
+
+Point pt = { 1,2 };
+
+Point f3()
+{
+	return pt;
+}
+
+Point& f4()    // return by reference : 리턴용 임시객체를 만들지 말고
+{			   // pt 자체를 반환해 달라는 의미(alias 반환) 
+	 
+	return pt;
+}
+
+int main()
+{
+//	f3().x = 10; // error. 임시 객체
+//	f3();
+//	f4();
+	f4().x = 10; // ok.. pt.x = 10 참조 반환이므로 임시객체 아님
+
+	std::cout << "-----" << std::endl;
+}
