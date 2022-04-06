4ddfe82f0aea7c09cf552cf8633324eb69d5dfdd
Domian <96786212+constantvision@users.noreply.github.com>
2022-04-06 15:31:47 +0900

    Add files via upload

---
+++ Day3/10_메뉴예제0.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+	printf("1. 김밥\n");
+	printf("2. 라면\n");
+	printf("3. 우동\n");
+	printf("메뉴를 선택 하세요 >> ");
+
+	int cmd;
+	scanf("%d", &cmd);
+
+	switch (cmd)
+	{
+	case 1: break;
+	case 2: break;
+	case 3: break;
+	}	
+}
