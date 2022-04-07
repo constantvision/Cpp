60594a5a45886c84937b87d1d7ddb5dd85cdad2b
Domian <96786212+constantvision@users.noreply.github.com>
2022-04-07 14:26:54 +0900

    Update 3_변수와타입6.cpp

--- Day1/3_변수와타입6.cpp
+++ Day1/3_변수와타입6.cpp
@@ -3,10 +3,10 @@
 
 int main()
 {
-	char s1[] = "ABCD";
+	char s1[] = "ABCD"; 
 	char s2[10];
 
-	s2 = s1;
+	s2 = s1;  ;// error. 배열의 이름은 상수 => strcpy를 써야함
 
-	if ( s1 == s2 ) {}
+	if ( s1 == s2 ) {} // strcmp를 써서 비교해야함 strcmp(s2,s1) == 0 
 }
